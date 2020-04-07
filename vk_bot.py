9942aba7fb96855b16da635163b732b85f746e8c
AdWanTay <khaliullin-73@mail.ru>
2020-04-07 23:20:21 +0500

    sdsadas

--- vk_bot.py
+++ vk_bot.py
@@ -43,8 +43,9 @@ while True:
                     message='ты хочешь биологию?\n Напиши "Да", если реально хочешь))/',
                     random_id=random.randint(-1000000000, 1000000000)
                 )
-                event = longPoll.listen()
             if event.text.lower() == "да":
-                user_id = event.user_id,
-                message = 'вопрос/',
-                random_id = random.randint(-1000000000, 1000000000)
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    message='вопрос/',
+                    random_id=random.randint(-1000000000, 1000000000)
+                )
