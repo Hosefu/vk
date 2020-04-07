4729677786870aca20c2cf3cad03650d3c63b802
Hosefu <hosefu@ya.u>
2020-04-07 22:26:02 +0500

    функционал!

--- vk_bot.py
+++ vk_bot.py
@@ -14,8 +14,18 @@ vk = vk_session.get_api()
 conn = sqlite3.connect(config.sqlite_path)
 c = conn.cursor()
 
+
+def get_user(user_id):
+    cmd = "SELECT * FROM users WHERE user_id=%d" % user_id
+    c.execute(cmd)
+    result = c.fetchone()
+    return result
+
+
 def register_new_user(user_id):
     cmd = "INSERT INTO users(user_id, state) VALUES (%d, '')" % user_id
+    c.execute(cmd)
+    conn.commit()
 
 
 
@@ -23,3 +33,12 @@ while True:
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
 
+            if get_user(user_id=event.user_id) is None:
+                register_new_user(user_id=event.user_id)
+
+            if event.text.lower() == "привет":
+                vk.messages.send(  # Отправляем сообщение
+                    user_id=event.user_id,
+                    message="Это Питон, детка!",
+                    random_id=random.randint(-1000000000, 1000000000)
+                )
